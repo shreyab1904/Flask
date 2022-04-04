26dd359ad0c445cf81bfa972b9ead4eff590abac
shreyab1904 <shreyaborkar0201@gmail.com>
2022-04-04 15:47:11 +0530

    Contact added

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app =  Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return "Welcome to my website"
+    return "<h1>WELCOME</h1>"
 
 @app.route("/contact")
 def Contact_page():
-    return "Contact Page"
+    return render_template("contact.html")
+
+@app.route("/home")
+def HomePage():
+    return "Home Page"
+
+@app.route("/gallery")
+def gallery():
+    return "Insert your pictures"
+
 
 if __name__ == "__main__":
     app.run()
